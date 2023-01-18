d441d8d7a3452d7329caabdcc1c2b1ade3dc6626
daniel-lgs <122944322+daniel-lgs@users.noreply.github.com>
2023-01-18 13:47:25 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+music_genre = input("What is your favorite music genre?\n")
+country_name = input("What is your country?\n")
+band_name = music_genre + " " + country_name
+print("Your band name is " + band_name)
